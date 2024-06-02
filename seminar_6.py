f6a1ac86cbfb0b084b5e1006af5fec642cd8cbe3
Rompetr78 <rompetr@yahoo.com>
2024-06-03 00:13:25 +0800

    Added the additional task #5 in seminar_6.py

--- seminar_6.py
+++ seminar_6.py
@@ -229,4 +229,27 @@ primary_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 print([primary_list[i] for i in range(1, len(primary_list)) if primary_list[i] > primary_list[i - 1]])
 
 #--------------------------------------
+# Дополнительное задание №5
+✔ Функция принимает на вход три списка одинаковой длины:
+✔ имена str,
+✔ ставка int,
+✔ премия str с указанием процентов вида «10.25%».
+✔ Вернуть словарь с именем в качестве ключа и суммой
+премии в качестве значения.
+✔ Сумма рассчитывается как ставка умноженная на процент премии.
+
+Решите через lc.
+names = ['Борис', 'Семен', 'Петр', "Сергей"]
+salaries = [10000, 12000, 16000, 14000]
+awards = ['12.35%', '10.25%', '7.75%', '8.85%']
+
+print(func(names, salaries, awards))
 """
+def func(names, salaries, awards):
+    return {name: salary * float(award.rstrip("%")) / 100 for name, salary, award in zip(names, salaries, awards)}
+
+names = ['Борис', 'Семен', 'Петр', "Сергей"]
+salaries = [10000, 12000, 16000, 14000]
+awards = ['12.35%', '10.25%', '7.75%', '8.85%']
+
+print(func(names, salaries, awards))
