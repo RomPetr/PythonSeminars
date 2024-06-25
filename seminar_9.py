59dd542f42e2883f379bd42c8e21a3da9eabd5b8
Rompetr78 <rompetr@yahoo.com>
2024-06-25 12:19:15 +0800

    added the HW2 in seminar_9.py

--- seminar_9.py
+++ seminar_9.py
@@ -73,3 +73,19 @@ filtered_df = df[(df['population'] >= 0) & (df['population'] <= 500)]
 avg = filtered_df['median_house_value'].mean()
 print(avg)
 """
+
+
+# HW 2
+"""
+Дан файл california_housing_train.csv.
+Найти максимальное значение переменной "households" в зоне минимального значения переменной population и 
+сохраните это значение в переменную max_households_in_min_population.
+Используйте модуль pandas.
+"""
+import pandas as pd
+
+# Загрузка данных из файла
+df = pd.read_csv('california_housing_test.csv')
+# print(data[data['median_house_value'] == data['median_house_value'].min()]['population'].max())
+max_households_in_min_population = df[df['population'] == df['population'].min()]['households'].max()
+print(max_households_in_min_population)
