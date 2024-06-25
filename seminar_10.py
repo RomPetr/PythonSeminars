bf668c68cc63db313ff132f836af0ca40d3d9249
Rompetr78 <rompetr@yahoo.com>
2024-06-25 23:36:22 +0800

    added Task 1 in seminar_10.py files

--- seminar_10.py
+++ seminar_10.py
@@ -1,6 +1,8 @@
 from pandas import read_csv
-from seaborn import scatterplot
+from seaborn import scatterplot, relplot, histplot
 from matplotlib.pyplot import show
+# import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
 
 data = read_csv("california_housing_test.csv")
 
@@ -18,4 +20,28 @@ def first():
     scatterplot(data=data, x='households', y='population')
     show()
 
-first()
+# first()
+
+#2. Визуализировать longitude по отношения к median_house_value, используя линейный график
+
+def second():
+    relplot(data=data, x='longitude', y='median_house_value', kind='line')
+    show()
+
+# second()
+
+# 3. Представить гистограмму по housing_median_age
+
+def third():
+    histplot(data=data, x='housing_median_age')
+    show()
+
+# third()
+
+# 4. Изобразить гистограмму по median_house_value с оттенком housing_median_age
+
+def fourth():
+    histplot(data=data, x='median_house_value', hue='housing_median_age')
+    show()
+
+fourth()
