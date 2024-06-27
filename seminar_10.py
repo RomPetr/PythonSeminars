f1709a9929bf09cdb136074f6f653a8377f31bfa
Rompetr78 <rompetr@yahoo.com>
2024-06-27 12:24:23 +0800

    added Task 4 in seminar_10.py

--- seminar_10.py
+++ seminar_10.py
@@ -119,15 +119,15 @@ f_5()
 """
 
 """
-Задача № 3. Решение в группах
+Задача №3. Решение в группах
 Создать новый столбец в таблице с пингвинами, который будет отвечать за
 показатель длины клюва пингвина.
 
 high - длинный(от 42)
 middle - средний(от 35 до 42)
 low - маленький(до 35)
+#--------------------------------------
 
-"""
 
 from pandas import read_csv
 
@@ -137,3 +137,20 @@ penguins.loc[penguins['bill_length_mm'] < 35, 'height_group'] = 'low'
 penguins.loc[(penguins['bill_length_mm'] > 35 & penguins['bill_length_mm'] < 42), 'height_group'] = 'middle'
 penguins.loc[penguins['bill_length_mm'] > 34, 'height_group'] = 'high'
 penguins.to_csv('penguins.csv', index=False)
+"""
+
+"""
+Задача №4. Решение в группах
+
+Изобразить гистограмму по flipper_length_mm
+с оттенком height_group. Сделать анализ
+"""
+
+from seaborn import histplot
+from pandas import read_csv
+from matplotlib.pyplot import show
+
+penguins = read_csv('penguins.csv')
+
+histplot(penguins, x='flipper_length_mm', hue='height_group')
+show()
